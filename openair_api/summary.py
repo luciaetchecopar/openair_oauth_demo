ee49dc024175a3504b68ad2d396852b3d9c316e6
ignacio-carrera <112402115+ignacio-carrera@users.noreply.github.com>
2025-05-13 20:16:36 -0300

    Update and rename summary.py to openair_api/summary.py

--- openair_api/summary.py
+++ openair_api/summary.py
@@ -1,27 +1,33 @@
+import os
 from collections import defaultdict
 import csv
 
 def summarize_billable_hours(entries):
     user_hours = defaultdict(float)
-
     for entry in entries:
         user_id = entry.get("userId")
         hours = entry.get("decimalHours", 0)
         user_hours[user_id] += hours
-
     return dict(user_hours)
 
 
 def save_summary_to_csv(summary, filename, start_date=None, end_date=None):
-    with open(filename, mode="w", newline="") as csv_file:
+    base_path = os.path.dirname(__file__)
+    full_path = os.path.abspath(os.path.join(base_path, filename))
+
+    with open(full_path, mode="w", newline="") as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(["userId", "total_billable_hours", "start_date", "end_date"])
         for user_id, total_hours in summary.items():
             writer.writerow([user_id, f"{total_hours:.2f}", start_date, end_date])
-    print(f"✅ Summary saved to {filename}")
+    print(f"✅ Summary saved to {full_path}")
+
 
 def save_users_to_csv(users, filename="users_summary.csv"):
-    with open(filename, mode="w", newline="") as csv_file:
+    base_path = os.path.dirname(__file__)
+    full_path = os.path.abspath(os.path.join(base_path, filename))
+
+    with open(full_path, mode="w", newline="") as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(["id", "firstName", "lastName", "email"])
 
@@ -33,5 +39,4 @@ def save_users_to_csv(users, filename="users_summary.csv"):
                 user.get("email", "")
             ])
 
-    print(f"✅ Users saved to {filename}")
-
+    print(f"✅ Users saved to {full_path}")
